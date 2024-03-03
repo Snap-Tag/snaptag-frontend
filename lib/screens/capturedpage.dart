85f8ac8f5fd9250484d2240d656f59d48014a96a
gaurovgiri <gaurovgiri@gmail.com>
2024-03-03 12:10:43 +0545

    feat: add provider

--- lib/screens/capturedpage.dart
+++ lib/screens/capturedpage.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:snaptag_frontend/services/network.dart';
 import 'package:snaptag_frontend/models/responseModel.dart';
 import 'package:snaptag_frontend/widgets/text_widget.dart';
+import 'package:snaptag_frontend/providers/tagsProvider.dart';
 
 class CapturedPage extends StatefulWidget {
   final String imagePath;
-  const CapturedPage({super.key, required this.imagePath});
+  const CapturedPage({Key? key, required this.imagePath}) : super(key: key);
 
   @override
   State<CapturedPage> createState() => _CapturedPageState();
@@ -18,87 +20,99 @@ class _CapturedPageState extends State<CapturedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: FutureBuilder<ResponseModel>(
-            future: SnapTagAPIRequest.getTags(widget.imagePath),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return Column(children: [
-                  Center(
-                    child: SizedBox(
-                      height: MediaQuery.of(context).size.height - 400,
-                      width: MediaQuery.of(context).size.width - 50,
-                      child: Card(
-                          child: Image.memory(
+      appBar: AppBar(),
+      body: FutureBuilder<ResponseModel>(
+        future: SnapTagAPIRequest.getTags(widget.imagePath),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            final tagsProvider =
+                Provider.of<TagsProvider>(context, listen: false);
+            tagsProvider
+                .setTag(snapshot.data!.extractedTags); // Set the tags initially
+
+            return Column(
+              children: [
+                Center(
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height - 400,
+                    width: MediaQuery.of(context).size.width - 50,
+                    child: Card(
+                      child: Image.memory(
                         base64Decode(snapshot.data!.croppedDocument),
                         fit: BoxFit.contain,
-                      )),
+                      ),
                     ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const TextWidget(text: "Tags"),
-                      IconButton(
-                          onPressed: () async {
-                            return showDialog(
-                              context: context,
-                              builder: (context) {
-                                return AlertDialog(
-                                  title: const Text('Add Tag'),
-                                  content: TextField(
-                                    controller: _textFieldController,
-                                    decoration: const InputDecoration(
-                                        hintText: "Enter Tag"),
-                                  ),
-                                  actions: <Widget>[
-                                    TextButton(
-                                      child: const Text('CANCEL'),
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                    ),
-                                    TextButton(
-                                      child: const Text('OK'),
-                                      onPressed: () {
-                                        setState(() {
-                                          snapshot.data!.extractedTags
-                                              .add(_textFieldController.text);
-                                        });
-                                        Navigator.pop(context);
-                                      },
-                                    ),
-                                  ],
-                                );
-                              },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const TextWidget(text: "Tags"),
+                    IconButton(
+                      onPressed: () async {
+                        return showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('Add Tag'),
+                              content: TextField(
+                                controller: _textFieldController,
+                                decoration: const InputDecoration(
+                                  hintText: "Enter Tag",
+                                ),
+                              ),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: const Text('CANCEL'),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                                TextButton(
+                                  child: const Text('OK'),
+                                  onPressed: () {
+                                    tagsProvider
+                                        .addTag(_textFieldController.text);
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                              ],
                             );
                           },
-                          icon: const Icon(Icons.add))
-                    ],
-                  ),
-                  SizedBox(
-                    height: 190,
-                    width: 250,
-                    child: ListView.builder(
-                      itemCount: snapshot.data!.extractedTags.length,
+                        );
+                      },
+                      icon: const Icon(Icons.add),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 190,
+                  width: 250,
+                  child: Consumer<TagsProvider>(
+                    builder: (context, tagsProvider, _) => ListView.builder(
+                      itemCount: tagsProvider.tags.length,
                       itemBuilder: (context, index) {
+                        final tag = tagsProvider.tags[index];
                         return ListTile(
-                          title: Text(snapshot.data!.extractedTags[index]),
+                          title: Text(tag),
                           trailing: IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  snapshot.data!.extractedTags.removeAt(index);
-                                });
-                              },
-                              icon: const Icon(Icons.remove)),
+                            onPressed: () {
+                              tagsProvider.removeTag(index);
+                            },
+                            icon: const Icon(Icons.remove),
+                          ),
                         );
                       },
                     ),
                   ),
-                ]);
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            }));
+                ),
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
   }
 }
