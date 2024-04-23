bb9849a51b8b4765756f96d4c26982be3bd9542a
gaurovgiri <gaurovgiri@gmail.com>
2024-04-23 21:26:21 +0545

    update: change server ip

--- lib/services/network.dart
+++ lib/services/network.dart
@@ -6,10 +6,10 @@ import 'package:snaptag_frontend/models/responseModel.dart';
 import 'package:http_parser/http_parser.dart';
 
 class SnapTagAPIRequest {
-  static const serverIP = "192.168.1.3:8000";
+  static const serverIP = "snaptag.anmup.online";
 
   static Future<ResponseModel> getTags(String imagePath) async {
-    const String url = "http://$serverIP/snapservice";
+    const String url = "https://$serverIP/snapservice";
     final dio = Dio();
 
     FormData formData = FormData.fromMap({
